687bb7f3a5f3c2d821cf66ae691a3e52fb18248f
Rejman <rreejjmmaann@gmail.com>
2019-03-14 22:56:18 +0100

    Add the window's look

--- src/main/java/myPck/Main.java
+++ src/main/java/myPck/Main.java
@@ -3,8 +3,7 @@ package myPck;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,9 +15,9 @@ public class Main extends Application {
 
     public void start(Stage primaryStage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/border.fxml"));
-        BorderPane borderPane = loader.load();
-        Scene scene = new Scene(borderPane);
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/MainWindow.fxml"));
+        StackPane stackPane = loader.load();
+        Scene scene = new Scene(stackPane);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Service station");
 
---
+++ src/main/java/myPck/controllers/MainWindowController.java
@@ -0,0 +1,79 @@
+package myPck.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class MainWindowController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TableView<?> servicesTableView;
+
+    @FXML
+    private TableColumn<?, ?> carColumn;
+
+    @FXML
+    private TableColumn<?, ?> clientColumn;
+
+    @FXML
+    private TableColumn<?, ?> statusColumn;
+
+    @FXML
+    private Button addNewServiceButton;
+
+    @FXML
+    private Button invoicePDFButton;
+
+    @FXML
+    private Button showDetailsButton;
+
+    @FXML
+    private Tab profileTab;
+
+    @FXML
+    private Tab adminPanelTab;
+
+    @FXML
+    private Tab tasksTab;
+
+    @FXML
+    void addServicesTest(ActionEvent event) {
+
+    }
+
+    @FXML
+    void invoicePDFTest(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showDetailsTest(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert servicesTableView != null : "fx:id=\"servicesTableView\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert carColumn != null : "fx:id=\"carColumn\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert clientColumn != null : "fx:id=\"clientColumn\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert statusColumn != null : "fx:id=\"statusColumn\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert addNewServiceButton != null : "fx:id=\"addNewServiceButton\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert invoicePDFButton != null : "fx:id=\"invoicePDFButton\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert showDetailsButton != null : "fx:id=\"showDetailsButton\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert profileTab != null : "fx:id=\"profileTab\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert adminPanelTab != null : "fx:id=\"adminPanelTab\" was not injected: check your FXML file 'MainWindow.fxml'.";
+        assert tasksTab != null : "fx:id=\"tasksTab\" was not injected: check your FXML file 'MainWindow.fxml'.";
+
+    }
+}
