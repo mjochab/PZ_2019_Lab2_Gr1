a7dd43be88308add23554a7111302f6c3d9e4c39
Rejman <rreejjmmaann@gmail.com>
2019-03-22 17:37:30 +0100

    Update controller

--- src/main/java/myPck/controllers/AddServiceController.java
+++ src/main/java/myPck/controllers/AddServiceController.java
@@ -1,7 +1,11 @@
 package myPck.controllers;
 
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+import java.io.IOException;
 
 public class AddServiceController {
 
@@ -11,6 +15,44 @@ public class AddServiceController {
     public void setMainStackPaneController(MainStackPaneController mainStackPaneController) {
         this.mainStackPaneController = mainStackPaneController;
     }
+    @FXML
+    private Button addNewCustomerButton;
+
+    @FXML
+    private ListView<?> customersListView;
+
+    @FXML
+    private Button addNewCarButton;
+
+    @FXML
+    private ListView<?> carsListView;
+
+    @FXML
+    private Button saveServiceButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    void addNewCar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addNewCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cancel(ActionEvent event) throws IOException {
+        mainStackPaneController.loadMainWindow();
+    }
+
+    @FXML
+    void saveService(ActionEvent event) throws IOException {
+        mainStackPaneController.loadMainWindow();
+    }
+
     @FXML
     void initialize() {
 
