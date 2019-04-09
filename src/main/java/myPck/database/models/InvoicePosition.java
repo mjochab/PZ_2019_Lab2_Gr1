4f23bb10e312d29a0bdc12e6edce8aeea54e8d11
Rejman <rreejjmmaann@gmail.com>
2019-04-09 19:01:29 +0200

    Refactor InvoicePosition model

--- src/main/java/myPck/database/models/InvoicePosition.java
+++ src/main/java/myPck/database/models/InvoicePosition.java
@@ -1,26 +1,52 @@
 package myPck.database.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
 @Table(name = "invoice_positions")
 public class InvoicePosition implements Serializable {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name = "name", length = 40, nullable = false)
     private String name;
+    @Column(name = "price", nullable = false)
     private double price;
 
     public InvoicePosition(){
-
     }
-
+    /**
+     *
+     * @param name
+     * @param price
+     */
     public InvoicePosition(String name, double price) {
         this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
     }
 }
