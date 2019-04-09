428801ebcd8154774c0376dad6399cab63d5d8aa
Rejman <rreejjmmaann@gmail.com>
2019-04-09 10:51:46 +0200

    Update ProfileController

--- src/main/java/myPck/controllers/ProfileController.java
+++ src/main/java/myPck/controllers/ProfileController.java
@@ -1,11 +1,17 @@
 package myPck.controllers;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import myPck.database.models.User;
+import myPck.modelsFx.UserFx;
+import myPck.services.UserService;
+
+import java.util.List;
 
 public class ProfileController {
 
@@ -39,6 +45,30 @@ public class ProfileController {
     @FXML
     void initialize() {
 
+        loadUser();
+        setUpUser();
+        convertUserToUserFx();
 
     }
+
+    private UserService userService;
+    private User user;
+    private UserFx userFx;
+
+    public ProfileController() {
+        userService = new UserService();
+    }
+    public void loadUser() {
+        user = userService.findById("3");
+    }
+
+    public void convertUserToUserFx() {
+       userFx = new UserFx(user.getEmail(), user.getFirstName(), user.getLastName(), user.getLogin(), user.getRole());
+    }
+    public void setUpUser() {
+
+        firstNameField.textProperty().bind(userFx.firstNameProperty());
+    }
+
+
 }
