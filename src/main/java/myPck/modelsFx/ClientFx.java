233bb71e947963d24a149e575c7440f6a08b4ec2
Rejman <rreejjmmaann@gmail.com>
2019-04-10 20:50:30 +0200

    Add database connect to AddServiceController

--- src/main/java/myPck/modelsFx/ClientFx.java
+++ src/main/java/myPck/modelsFx/ClientFx.java
@@ -3,16 +3,17 @@ package myPck.modelsFx;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
-public class CarFx {
+public class ClientFx {
     private StringProperty name = new SimpleStringProperty();
 
-    public CarFx(String name) {
+    public ClientFx(String name) {
         this.name.set(name);
     }
 
     public StringProperty nameProperty() {
         return name;
     }
+
     @Override
     public String toString() {
         return name.toString();
