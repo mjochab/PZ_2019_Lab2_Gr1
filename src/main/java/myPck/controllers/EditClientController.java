e743f2406f3efde147cf41e8a56c9c9df825753a
Rejman <rreejjmmaann@gmail.com>
2019-04-11 19:20:09 +0200

    Add back() method
    I19L2GR1-7

--- src/main/java/myPck/controllers/EditClientController.java
+++ src/main/java/myPck/controllers/EditClientController.java
@@ -1,8 +1,15 @@
 package myPck.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import myPck.database.models.Client;
+
+import java.io.IOException;
 
 public class EditClientController extends Controller{
 
@@ -31,6 +38,19 @@ public class EditClientController extends Controller{
     @FXML
     void initialize() {
 
+
+    }
+    @FXML
+    void back(ActionEvent event) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/AddService.fxml"));
+        StackPane stackPane = loader.load();
+
+        /** przekazanie kontrolera (głównego okna) do okienka AddService */
+        AddServiceController addServiceController = loader.getController();
+        addServiceController.setMainStackPaneController(mainStackPaneController);
+        /** Ustawienie okna AddService */
+        mainStackPaneController.setScreen(stackPane);
 
     }
 }
