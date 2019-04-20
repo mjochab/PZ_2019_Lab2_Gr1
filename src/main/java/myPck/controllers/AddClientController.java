58b7fd1b202956fd1cb498783e435dd648b14ab8
Rejman <rreejjmmaann@gmail.com>
2019-04-20 20:05:22 +0200

    Refactor AddClientController.java

--- src/main/java/myPck/controllers/AddClientController.java
+++ src/main/java/myPck/controllers/AddClientController.java
@@ -15,36 +15,28 @@ import java.io.IOException;
 
 public class AddClientController extends Controller {
 
-
     @FXML
     private TextField firstNameField;
-
     @FXML
     private TextField lastNameField;
-
     @FXML
     private TextField NIPNumberField;
-
     @FXML
     private TextField addressField;
-
     @FXML
     private Button addButton;
-
     @FXML
     private Button backButton;
-
     @FXML
     void add(ActionEvent event) throws IOException {
-
         String firstName = firstNameField.getText();
         String lastName = lastNameField.getText();
         String nipNumber = NIPNumberField.getText();
         String address = addressField.getText();
 
-        if(firstName.isEmpty() || lastName.isEmpty() || address.isEmpty() || nipNumber.isEmpty() || nipNumber.length()!=10){
+        if (firstName.isEmpty() || lastName.isEmpty() || address.isEmpty() || nipNumber.isEmpty() || nipNumber.length() != 10) {
             System.out.println("Error");
-        }else {
+        } else {
 
             this.client = new Client(firstName, lastName, nipNumber, address);
             clientService.persist(this.client);
@@ -52,9 +44,7 @@ public class AddClientController extends Controller {
             backButton.fire();
         }
 
-
     }
-
     @FXML
     void back(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/AddService.fxml"));
@@ -77,6 +67,5 @@ public class AddClientController extends Controller {
     private Client client;
     private ClientFx clientFx;
     private ClientService clientService;
-
 
 }
