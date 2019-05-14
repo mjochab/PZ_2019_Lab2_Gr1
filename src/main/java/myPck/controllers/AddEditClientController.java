d7d310d080d66bcb1fcb29933701a78cf025549d
FilipRebizant <filiprebizant@gmail.com>
2019-05-14 20:01:55 +0200

    Install JUnit and write some tests

--- src/main/java/myPck/controllers/AddEditClientController.java
+++ src/main/java/myPck/controllers/AddEditClientController.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
-import myPck.controllers.utils.Validator;
+import myPck.utils.Validator;
 import myPck.database.models.Client;
 import myPck.modelsFx.ClientFx;
 import myPck.services.ClientService;
