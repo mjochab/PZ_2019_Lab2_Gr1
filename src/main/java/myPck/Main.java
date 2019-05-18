dcfd0a30dd92145903ebd0881655df5b3b2a3370
Rejman <rreejjmmaann@gmail.com>
2019-05-18 13:36:00 +0200

    Add company model

--- src/main/java/myPck/Main.java
+++ src/main/java/myPck/Main.java
@@ -5,7 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import myPck.database.models.Company;
 import myPck.database.utils.DbManager;
+
+import java.io.*;
 
 public class Main extends Application {
 
@@ -19,12 +22,12 @@ public class Main extends Application {
         Scene scene = new Scene(stackPane, 700, 500);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Service station");
-
         primaryStage.show();
+
 
         /** Instancja DbManagera potrzebna to uzupełnienia bazy danymi.**/
         DbManager dbManager = new DbManager();
         /**Uzupełnienie bazy przykładowymi danymi */
-//        dbManager.addSampleData();
+        //dbManager.addSampleData();
     }
 }
